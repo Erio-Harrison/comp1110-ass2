b67f3a2a9340cb07df7c463f66d209d8ae05bf99
wenxuan Cao <u7556970@anu.edu.au>
2023-05-17 11:52:50 +1000

    ValidMovesTest

--- tests/comp1110/ass2/ValidMovesTest.java
+++ tests/comp1110/ass2/ValidMovesTest.java
@@ -1,8 +1,17 @@
 package comp1110.ass2;
+import comp1110.ass2.testdata.AllValidMovesDataLoader;
+import comp1110.ass2.testdata.DataLoader;
+import comp1110.ass2.testdata.GameDataLoader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
-public class CheckEndTest {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class ValidMovesTest{
     public static final String DEFAULT_GAME = "a 13 2; c 0 E; i 6 0,0 0,1 0,2 0,3 1,0 1,1 1,2 1,3 1,4 2,0 2,1; i 6 0,5 0,6 0,7 1,6 1,7 1,8 2,6 2,7 2,8 3,7 3,8; i 6 7,12 8,11 9,11 9,12 10,10 10,11 11,10 11,11 11,12 12,10 12,11; i 8 0,9 0,10 0,11 1,10 1,11 1,12 2,10 2,11 3,10 3,11 3,12 4,10 4,11 5,11 5,12; i 8 4,0 5,0 5,1 6,0 6,1 7,0 7,1 7,2 8,0 8,1 8,2 9,0 9,1 9,2; i 8 10,3 10,4 11,0 11,1 11,2 11,3 11,4 11,5 12,0 12,1 12,2 12,3 12,4 12,5; i 10 3,3 3,4 3,5 4,2 4,3 4,4 4,5 5,3 5,4 5,5 5,6 6,3 6,4 6,5 6,6 7,4 7,5 7,6 8,4 8,5; i 10 5,8 5,9 6,8 6,9 7,8 7,9 7,10 8,7 8,8 8,9 9,7 9,8 9,9 10,6 10,7 10,8 11,7 11,8 12,7 12,8; s 0,0 0,5 0,9 1,4 1,8 1,12 2,1 3,5 3,7 3,10 3,12 4,0 4,2 5,9 5,11 6,3 6,6 7,0 7,8 7,12 8,2 8,5 9,0 9,9 10,3 10,6 10,10 11,0 11,5 12,2 12,8 12,11; r C B W P S; p 0 0 0 0 0 0 0 S T; p 1 0 0 0 0 0 0 S T;";
     public static final String WHEELS_GAME = "a 13 2; c 0 E; i 5 0,1 0,2 0,3 0,4 1,1 1,5 2,0 2,5 3,0 3,6 4,0 4,5 5,1 5,5 6,1 6,2 6,3 6,4; i 5 0,8 0,9 0,10 1,8 1,11 2,7 2,11 3,8 3,11 4,8 4,9 4,10; i 7 8,8 8,9 8,10 9,8 9,11 10,7 10,11 11,8 11,11 12,8 12,9 12,10; i 7 10,0 10,1 10,4 10,5 11,0 11,2 11,3 11,4 11,6 12,0 12,1 12,4 12,5; i 9 2,2 2,3 3,2 3,4 4,2 4,3; i 9 2,9; i 9 6,6 6,7 6,8 6,9 6,10 6,11 7,6 8,0 8,1 8,2 8,3 8,4 8,5; i 9 10,9; s 0,1 0,4 0,10 2,2 2,3 2,9 2,11 3,0 3,2 3,4 3,6 4,2 4,3 4,10 6,1 6,4 6,6 6,11 8,0 8,5 8,8 8,10 10,0 10,5 10,7 10,9 10,11 11,3 12,1 12,4 12,8 12,10; r C B W P S; p 0 0 0 0 0 0 0 S T; p 1 0 0 0 0 0 0 S T;";
     public static final String FACE_GAME = "a 13 2; c 0 E; i 6 0,0 0,1 0,2 0,3 0,4 0,5 0,6 0,7 0,8 0,9 0,10 0,11 1,0 1,12 2,0 2,11 3,0 3,12 4,0 4,11 5,0 5,12 6,0 6,11 7,0 7,12 8,0 8,11 9,0 9,12 10,0 10,11 11,0 11,12 12,0 12,1 12,2 12,3 12,4 12,5 12,6 12,7 12,8 12,9 12,10 12,11; i 6 2,4 2,5 2,6 2,7; i 9 4,4 4,5 4,6 4,7; i 9 6,5 6,6 7,5 7,7 8,5 8,6; i 12 2,2 3,2 3,3 4,2 5,2 5,3 6,2 7,2 7,3; i 12 2,9 3,9 3,10 4,9 5,9 5,10 6,9 7,9 7,10; i 12 9,2 9,10 10,2 10,3 10,4 10,5 10,6 10,7 10,8 10,9; s 0,3 0,8 1,0 1,12 2,2 2,4 2,7 2,9 4,2 4,5 4,6 4,9 5,0 5,12 6,2 6,5 6,6 6,9 8,0 8,5 8,6 8,11 9,2 9,10 10,3 10,5 10,6 10,8 11,0 11,12 12,4 12,7; r C B W P S; p 0 0 0 0 0 0 0 S T; p 1 0 0 0 0 0 0 S T;";
@@ -12,88 +21,89 @@ public class CheckEndTest {
     public static final String SETTLE_GAME= "a 13 2; c 0 S; i 5 0,1 0,2 0,3 0,4 1,1 1,5 2,0 2,5 3,0 3,6 4,0 4,5 5,1 5,5 6,1 6,2 6,3 6,4; i 5 0,8 0,9 0,10 1,8 1,11 2,7 2,11 3,8 3,11 4,8 4,9 4,10; i 7 8,8 8,9 8,10 9,8 9,11 10,7 10,11 11,8 11,11 12,8 12,9 12,10; i 7 10,0 10,1 10,4 10,5 11,0 11,2 11,3 11,4 11,6 12,0 12,1 12,4 12,5; i 9 2,2 2,3 3,2 3,4 4,2 4,3; i 9 2,9; i 9 6,6 6,7 6,8 6,9 6,10 6,11 7,6 8,0 8,1 8,2 8,3 8,4 8,5; i 9 10,9; s 0,1 0,4 0,10 2,2 2,3 2,9 2,11 3,0 3,2 3,4 3,6 4,2 4,3 4,10 6,1 6,4 6,6 6,11 8,0 8,5 8,8 8,10 10,0 10,5 10,7 10,9 10,11 11,3 12,1 12,4 12,8 12,10; r C 0,1 0,10 4,2 8,10 11,3 B 0,4 2,2 6,6 8,5 10,9 12,10 W 3,0 3,4 3,6 4,3 6,11 P 2,3 3,2 8,0 10,7 12,1 12,4 S 2,9 2,11 6,1 10,0 10,5 10,11 12,8; p 0 34 0 0 0 0 1 S 1,5 5,5 6,4 8,7 9,7 9,9 T 2,5 4,5 7,6 9,8; p 1 42 1 0 1 0 0 S 3,10 4,10 7,9 7,10 8,8 8,9 T 3,11 6,10;";
     public static final String SETTLE_SIDES_GAME="a 7 2; c 1 S; i 4 0,0 0,1 0,2 0,3 1,0 1,1 1,2 1,3 2,0 2,1 2,2 2,3 3,0 3,1 3,2 3,3 4,0 4,1 4,2 4,3 5,0 5,1 5,2 5,3 6,0 6,1 6,2 6,3; i 20 0,5 1,5 1,6 2,5 3,5 3,6 4,5 5,5 5,6 6,5; s 0,0 0,1 0,2 0,3 1,1 1,2 1,3 1,5 1,6 2,0 2,1 2,2 2,3 3,0 3,1 3,2 3,3 3,5 3,6 4,0 4,1 4,2 4,3 5,1 5,2 5,3 5,5 5,6 6,0 6,1 6,2 6,3; r C B W P S; p 0 53 1 1 2 3 3 S 0,4 1,5 1,6 2,2 2,3 2,5 3,3 3,4 3,6 4,3 4,4 5,4 5,5 5,6 6,3 6,4 6,5 T 0,5 4,5; p 1 128 5 5 4 3 5 S 0,0 0,1 0,2 0,3 1,1 1,2 1,3 1,4 2,0 2,1 2,4 3,0 3,1 3,2 3,5 4,0 4,1 4,2 5,0 5,1 5,2 5,3 6,0 6,1 6,2 T 1,0;";
     public static final String MOVES_END_GAME = "a 13 2; c 1 E; i 6 0,0 0,1 0,2 0,3 1,0 1,1 1,2 1,3 1,4 2,0 2,1; i 6 0,5 0,6 0,7 1,6 1,7 1,8 2,6 2,7 2,8 3,7 3,8; i 6 7,12 8,11 9,11 9,12 10,10 10,11 11,10 11,11 11,12 12,10 12,11; i 8 0,9 0,10 0,11 1,10 1,11 1,12 2,10 2,11 3,10 3,11 3,12 4,10 4,11 5,11 5,12; i 8 4,0 5,0 5,1 6,0 6,1 7,0 7,1 7,2 8,0 8,1 8,2 9,0 9,1 9,2; i 8 10,3 10,4 11,0 11,1 11,2 11,3 11,4 11,5 12,0 12,1 12,2 12,3 12,4 12,5; i 10 3,3 3,4 3,5 4,2 4,3 4,4 4,5 5,3 5,4 5,5 5,6 6,3 6,4 6,5 6,6 7,4 7,5 7,6 8,4 8,5; i 10 5,8 5,9 6,8 6,9 7,8 7,9 7,10 8,7 8,8 8,9 9,7 9,8 9,9 10,6 10,7 10,8 11,7 11,8 12,7 12,8; s 0,0 0,5 0,9 1,4 1,8 1,12 2,1 3,5 3,7 3,10 3,12 4,0 4,2 5,9 5,11 6,3 6,6 7,0 7,8 7,12 8,2 8,5 9,0 9,9 10,3 10,6 10,10 11,0 11,5 12,2 12,8 12,11; r C 1,8 3,12 7,12 12,8 12,11 B 0,9 3,7 3,10 9,0 W 1,12 4,0 4,2 10,6 12,2 P 0,0 5,9 9,9 S 0,5 6,3 7,0 7,8 8,5 10,10; p 0 0 1 2 1 0 1 S 0,3 0,4 0,8 1,4 2,5 2,9 3,4 3,5 3,9 3,11 4,10 5,7 5,10 5,11 6,9 7,1 7,3 7,4 7,11 8,2 8,3 8,6 8,7 9,3 9,4 9,6 9,8 10,2 10,3 11,2 T 0,6 0,7 1,6 4,5 8,1; p 1 0 0 0 0 3 1 S 0,10 0,11 1,5 1,9 1,10 2,1 2,3 2,6 3,2 3,6 4,1 4,6 4,9 5,1 5,2 5,5 6,6 6,11 8,10 9,1 9,10 9,11 10,0 10,5 11,4 11,5 11,6 11,7 12,3 12,4 T 5,6 6,1 11,0 11,1 12,5;";
-    public void applyAllResourcesCollected(String gameString,boolean expected){
-        Model test = new Model();
-        test.toModel(gameString);
-        boolean actual = test.board.allResourcesCollected();
-        Assertions.assertEquals(expected,actual);
-    }
+    private final GameDataLoader gameDataLoader = new GameDataLoader();
+    private final AllValidMovesDataLoader loader = new AllValidMovesDataLoader();
 
-    //test the AllResourcesCollected method
-    //check if all the resources are collected or not
+    //test noValidMoves in Board class
 
-    @Test
-    public void testAllResourceCollected(){
-        //true case
-        applyAllResourcesCollected(DEFAULT_GAME,true);
-        applyAllResourcesCollected(WHEELS_GAME,true);
-        applyAllResourcesCollected(FACE_GAME,true);
-        applyAllResourcesCollected(SIDES_GAME,true);
-        applyAllResourcesCollected(SPACE_INVADERS_GAME,true);
-        applyAllResourcesCollected(SETTLE_SIDES_GAME,true);
-
-        //false case
-        applyAllResourcesCollected(NORMAL_GAME,false);
-        applyAllResourcesCollected(SETTLE_GAME,false);
-        applyAllResourcesCollected(MOVES_END_GAME,false);
-    }
-
-
-    //check if a player has valid moves or not
-    public void applyNoValidMoves(String gameState,boolean expected){
+    //a helper function to test actual and expected answer
+    public void applyMoveString(String gameState, Boolean expected){
         Model test = new Model();
         test.toModel(gameState);
-        boolean actual = test.board.noValidMoves(test.gamestate);
+        Boolean actual = test.board.noValidMoves(test.gamestate);
         Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testValidMoves(){
+        applyMoveString(DEFAULT_GAME,false);
+        applyMoveString(WHEELS_GAME,false);
+        applyMoveString(SIDES_GAME,false);
+        applyMoveString(FACE_GAME,false);
+        applyMoveString(SPACE_INVADERS_GAME,false);
+        applyMoveString(SETTLE_GAME,false);
+        applyMoveString(NORMAL_GAME,false);
+        applyMoveString(SETTLE_SIDES_GAME,false);
+
     }
 
     @Test
     public void testNoValidMoves(){
-        //players still have valid moves
-        applyNoValidMoves(DEFAULT_GAME,false);
-        applyNoValidMoves(WHEELS_GAME,false);
-        applyNoValidMoves(FACE_GAME,false);
-        applyNoValidMoves(SIDES_GAME,false);
-        applyNoValidMoves(SPACE_INVADERS_GAME,false);
-        applyNoValidMoves(NORMAL_GAME,false);
-        applyNoValidMoves(SETTLE_GAME,false);
-        applyNoValidMoves(SETTLE_SIDES_GAME,false);
-        //players have no valid moves
-        applyNoValidMoves(MOVES_END_GAME,true);
+        applyMoveString(MOVES_END_GAME,true);
     }
 
-    public void applyCheckEnd(String gameState,Boolean expected){
+
+    //test allValidMoves in Model class
+
+    //turn hashset into set
+    public static HashSet<String> setToHashSet(Set<String> set) {
+        return new HashSet<>(set);
+    }
+    //helper function of assertion test
+    public void test(HashSet<String> expected,String gameState){
         Model test = new Model();
         test.toModel(gameState);
-        boolean actual = test.checkEnd();
+        HashSet<String> actual = test.allValidMoves(test.currentPlayer);
         Assertions.assertEquals(expected,actual);
     }
 
-    @Test
-    public void testCheckEnd(){
-        //DEFAULT_GAME: all resources collected, still have valid moves
-        applyCheckEnd(DEFAULT_GAME,true);
-        //WHEELS_GAME: all resources collected, still have valid moves
-        applyCheckEnd(WHEELS_GAME,true);
-        //FACE_GAME: all resources collected, still have valid moves
-        applyCheckEnd(FACE_GAME,true);
-        //SIDES_GAME: all resources collected, still have valid moves
-        applyCheckEnd(SIDES_GAME,true);
-        //SPACE_INVADERS_GAME: all resources collected, still have valid moves
-        applyCheckEnd(SPACE_INVADERS_GAME,true);
-        //SETTLE_SIDES_GAME: all resources collected, still have valid moves
-        applyCheckEnd(SETTLE_SIDES_GAME,true);
-        //NORMAL_GAME: all resources not collected, still have valid moves
-        applyCheckEnd(NORMAL_GAME,false);
-        //SETTLE_GAME: all resources not collected, still have valid moves
-        applyCheckEnd(SETTLE_GAME,false);
-        //SETTLE_SIDES_GAME: all resources collected, still have valid moves
-        applyCheckEnd(SETTLE_SIDES_GAME,true);
-        //MOVES_END_GAME: all resources not collected, no valid moves
-        applyCheckEnd(MOVES_END_GAME,true);
+    //get the game solution
+    private void testGame(List<String> game, List<Set<String>> solutions){
+        for (int i = 0; i < solutions.size(); i++) {
+            test(setToHashSet(solutions.get(i)), game.get(2*i));
+        }
     }
 
+    public void testMapNamePlayerCount(String mapName, int playerCount){
+        List<List<String>> games = gameDataLoader.fetchGames(mapName, playerCount);
+        List<List<Set<String>>> solutions = loader.fetchGames(mapName, playerCount);
+        for(int game = 0; game < DataLoader.GAME_COUNT; game++){
+            testGame(games.get(game), solutions.get(game));
+        }
+    }
 
+    @Test
+    public void testDefaultTwoPlayerGames(){
+        testMapNamePlayerCount("default", 2);
+    }
+
+    @Test
+    public void testWheelsTwoPlayerGames(){
+        testMapNamePlayerCount("wheels", 2);
+    }
+
+    @Test
+    public void testFaceTwoPlayerGames(){
+        testMapNamePlayerCount("face", 2);
+    }
+
+    @Test
+    public void testEdgeCaseGames(){
+        List<List<String>> games = gameDataLoader.fetchAllEdgeCaseGames();
+        List<List<Set<String>>> solutions = loader.fetchAllEdgeCaseGames();
+        for(int game = 0; game < games.size(); game++){
+            testGame(games.get(game), solutions.get(game));
+        }
+    }
 
 
 }
